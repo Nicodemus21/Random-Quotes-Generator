3d169fdc1b250fd2ca571cbd3cbf460f793fd3b0
Nicodemus21 <45204936+Nicodemus21@users.noreply.github.com>
2019-07-12 09:50:36 +0300

    Add files via upload
    Gives Ellen White's quotes

---
+++ quotes.js
@@ -0,0 +1,106 @@
+// object literal:
+/* this array contains 5 quotes
+Objects in array include quote and source properties. If known, citation and date properties are present
+Objects in array include additional properties, such as categorization tags
+The quotes array should be accessible in the global scope. */
+
+var quotes = [
+  {
+    quote: "We are all woven together in the great web of humanity, and whatever we can do to benefit and uplift others will reflect in blessing upon ourselves.", 
+    source: "Ellen G. White",
+    citation: "Steps to Christ",
+    date: "1932",
+    tags: ["life","inspirational"]
+},
+  {
+    quote: "We need to have far less confidence in what man can do and far more confidence in what God can do for every believing soul. He longs to have you reach after Him by faith. He longs to have you expect great things from Him. He longs to give you understanding in temporal as well as in spiritual matters. He can sharpen the intellect. He can give tact and skill. Put your talents into the work, ask God for wisdom, and it will be given you.",
+    source: "Ellen G. White",
+    citation: "Christ's Object Lessons",
+    date: "1928",
+    tags: ["love", "death"]
+  },
+  {
+    quote: "The greatest want of the world is the want of men - men who will not be bought or sold, men who in their inmost souls are true and honest, men who do not fear to call sin by its right name, men whose conscience is as true to duty as the needle to the pole, men who will stand for the right though the heavens fall." ,
+    source: "Ellen G. White",
+    citation: "In Heavenly Places",
+    date: "1913",
+    tags: ["books", "education", "knowledge"]
+  },
+  {
+    quote: "The closer you come to Jesus, the more faulty you will appear in your own eyes; for your vision will be clearer, and your imperfections will be seen in broad and distinct contrast to His perfect nature." ,
+    source: "Ellen G. White",
+    date: "1914",
+    tags: ["anger", "change"]
+  },
+  {
+    quote: "A life in Christ is a life of restfulness. There may be no ecstasy of feeling, but there should be an abiding peaceful trust. Your hope is not in yourself; it is in Christ. Your weakness is united to His strength, your ignorance to His wisdom, your frailty to His enduring might....Let the mind dwell upon His love, upon the beauty, the perfection of His character.",
+    source: "Ellen G. White",
+    citation: "Steps to Christ",
+    date: "1926",
+    tags: ["comeback", "life"]
+  },
+  {
+    quote: "We have nothing to fear for the future, except as we shall forget the way the Lord has led us, and His teaching in our past history.",
+    source: "Ellen G. White",
+    tags: ["friendship", "bravery"]
+  },
+  {
+    quote: "Remember that you will never reach a higher standard than you yourself set. Then set your mark high, and step by step, even though it be by painful effort, by self-denial and sacrifice, ascend the whole length of the ladder of progress.",
+    source: "Ellen G. White",
+    tags: ["luck", "foolish"]
+  },
+  {
+    quote:  "Talk unbelief, and you will have unbelief; but talk faith, and you will have faith. According to the seed sown will be the harvest." ,
+    source: "Ellen G. White",
+    date: "1914",
+    tags: ["anger", "change"]
+  },
+  {
+    quote: "Whatever attracts the mind from God, whatever draws the affections away from Christ, is an enemy to the soul." ,
+    source: "Ellen G. White",
+    date: "1914",
+    tags: ["anger", "change"]
+  },
+  {
+    quote: "Our heavenly Father has a thousand ways to provide for us, of which we know nothing." ,
+    source: "Ellen G. White",
+    date: "1914",
+    tags: ["anger", "change"]
+  },
+  {
+    quote: "There are Christians who think and speak altogether too much about the power of Satan. They think of their adversary, they pray about him, they talk about him, and he looms up greater and greater in their imagination. It is true that Satan is a powerful being; but, thank God, we have a mighty Saviour, who cast out the evil one from heaven. Satan is pleased when we magnify his power. Why not talk of Jesus? Why not magnify His power and His love?" ,
+    source: "Ellen G. White",
+    date: "1914",
+    tags: ["anger", "change"]
+  },
+  {
+    quote: "Our heavenly Father has a thousand ways to provide for us, of which we know nothing." ,
+    source: "Ellen G. White",
+    date: "1914",
+    tags: ["anger", "change"]
+  },
+  {
+    quote: "Prayer is the opening of the heart to God as to a friend. Not that it is necessary in order to make known to God what we are, but in order to enable us to receive Him. Prayer does not bring God down to us, but brings us up to Him." ,
+    source: "Ellen G. White",
+    date: "1914",
+    tags: ["anger", "change"]
+  },
+  {
+    quote: "All true obedience comes from the heart. It was heart work with Christ. And if we consent, He will so identify Himself with our thoughts and aims, so blend our hearts and minds into conformity to His will, that when obeying Him we shall be but carrying out our own impulses. The will, refined and sanctified, will find its highest delight in doing His service. When we know God as it is our privilege to know Him, our life will be a life of continual obedience. Through an appreciation of the character of Christ, through communion with God, sin will become hateful to us." ,
+    source: "Ellen G. White",
+    date: "1914",
+    tags: ["anger", "change"]
+  },
+  {
+    quote: "There is no exercise that can take the place of walking. by it the circulation of the blood is greatly improved. Walking, in all cases where it is possible, is the best remedy for the diseased bodies, because in this, all of the organs of the body are brought into use." ,
+    source: "Ellen G. White",
+    date: "1914",
+    tags: ["anger", "change"]
+  },
+  {
+    quote: "It is Satan's work to fill men's hearts with doubt. He leads them to look upon God as a stern judge. He tempts them to sin, and then to regard themselves as too vile to approach their heavenly Father or to excite His pity. The Lord understands all this. Jesus assures His disciples of God's sympathy for them in their needs and weaknesses. Not a sigh is breathed, not a pain felt, not a grief pierces the soul, but the throb vibrates to the Father's heart." ,
+    source: "Ellen G. White",
+    date: "1914",
+    tags: ["anger", "change"]
+  },
+];
---
+++ script.js
@@ -0,0 +1,78 @@
+var quotesCopy = [];
+
+/* Event listener to respond to "Show another quote" button clicks.
+When user clicks anywhere on the button, the "printQuote" function is called.
+Found information about intervals and timers on https://javascriptweblog.wordpress.com/2010/06/28/understanding-javascript-timers/ */
+document.getElementById("loadQuote").addEventListener("click", function() {
+  timer();
+  printQuote();
+});
+
+window.onload = printQuote();
+
+// This function only goes to A so that the colour will most likely be darker and the text remains legible.
+function getRandomColor() {
+  var letters = "0123456789A";
+  var colour = "#";
+  for (var i = 0; i < 6; i++) {
+    colour += letters[Math.floor(Math.random() * letters.length)];
+  }
+  return colour;
+}
+
+/* This function checks to see if quotesCopy is empty, and if it is empty, it
+clones the quotes array into the quotesCopy array. It then splices a random object
+from the quotesCopy array, and returns it. It does not return a duplicate quote until
+all quotes have been returned once. */
+function getRandomQuote() {
+  if (quotesCopy.length === 0) {
+    quotesCopy = [].concat(quotes);
+  }
+  var quote = quotesCopy.splice(
+    Math.floor(Math.random() * quotesCopy.length),
+    1
+  )[0];
+  // this log has been left in to show that the copy is being used.
+  console.log(quotesCopy);
+  return quote;
+}
+
+// Timer
+function timer() {
+  clearInterval(window.intervalID);
+  window.intervalID = setInterval(printQuote, 60000);
+}
+timer();
+
+// Changes background and button colors
+function changeColors() {
+  document.getElementById(
+    "loadQuote"
+  ).style.backgroundColor = document.body.style.backgroundColor = getRandomColor();
+}
+/* this function calls the randomQuote function and prints the quote to the page
+using the template supplied in the project instructions.
+Background and button colors change each time the quote changes by calling changeColors.
+printQuote doesn't add a for a missing citation or a if the year property is missing
+ Looked at CSS Properties Reference to find which property to use to
+access backgroundColor https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_Properties_Reference
+Quotes change automatically after 30 seconds passes by calling timer.
+*/
+
+function printQuote() {
+  var currentQuote = getRandomQuote();
+  var message = '<p class="quote">' + currentQuote.quote + "</p>";
+  message += '<p class="source">' + currentQuote.source;
+  if (currentQuote.citation) {
+    message += '<span class="citation">' + currentQuote.citation + "</span>";
+  }
+  if (currentQuote.date) {
+    message += '<span class="year">' + currentQuote.date + "</span>";
+  }
+  message += "</p>";
+
+  document.getElementById("quote-box").innerHTML = message;
+  changeColors();
+}
+
+printQuote();
